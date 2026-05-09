aeb043437eef7745d36ebf491a5744d6a324490e
fizazi200 <fizazi2021@gmail.com>
2026-05-09 17:00:24 +0200

    initial spring boot project structure du portail unifié

---
+++ src/main/java/com/honeygroup/portailunifier/PortailUnifierApplication.java
@@ -0,0 +1,13 @@
+package com.honeygroup.portailunifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortailUnifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortailUnifierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/honeygroup/portailunifier/PortailUnifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.honeygroup.portailunifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortailUnifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
